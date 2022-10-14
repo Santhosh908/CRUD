b7d4d83c7929fe6fae8e8b555bed639871c0d14d
Santhosh908 <86720495+Santhosh908@users.noreply.github.com>
2022-10-14 10:20:27 +0530

    changes

--- back-end/index.js
+++ back-end/index.js
@@ -13,7 +13,6 @@ app.get('/', function(req, res) {
 
 app.post("/adddetails",(req,res)=>{
     var val=req.body;
-    var a=val[0]
     var querry=`INSERT INTO neoproject.userdetails(name,email,num,date1,location,gender,address) VALUES("${val[0]}","${val[1]}","${val[2]}","${val[3]}","${val[4]}","${val[5]}","${val[6]}")`;
     database.query(querry,function(err,rows){
         if(err)
@@ -99,6 +98,28 @@ app.post("/byloc",(res,req)=>{
         res.send(rows);
     })
 })
+app.post("/searchdetails",(req,res)=>{
+    var data1=[];
+    // console.log(req.body);
+    var querry=`SELECT * FROM neoproject.userdetails WHERE id="${req.body[0]}"`;
+    database.query(querry,function(err,rows){
+        if(err)
+        throw err
+        else{
+            res.send(rows)
+        }
+    })
+    
+});
+app.post("/updatedetails",(req,res)=>{
+    var val=req.body;
+    var querry=`UPDATE neoproject.userdetails SET name="${val[0]}",email="${val[1]}",num="${val[2]}",date1="${val[3]}",location="${val[4]}",gender="${val[5]}",address="${val[6]}" WHERE id="${val[7]}"`;
+    database.query(querry,function(err,rows){
+        if(err)
+        throw err
+        
+    })
+});
 app.listen(PORT, function(){
     console.log("Server running on localhost:" + PORT);
   });
